53e1f72d4d453dc683f94088ed88075cb8f6faa7
Sourabh Undale <sourabhundale@gmail.com>
2024-08-23 16:36:07 +0530

    megha project

--- ReactJS/12MegaBlog/src/appwrite/config.js
+++ ReactJS/12MegaBlog/src/appwrite/config.js
@@ -8,9 +8,127 @@ export class Service{
     bucket;
 
     constructor(){
+        this.client
+        .setEndpoint(conf.appwriterUrl)
+        .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
         
     }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await thsis.databases.createDocument(
+                conf.appwritDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )             
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+    async updatePost( slug,{title, content, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwritDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+
+                }
+            );            
+        } catch (error) {
+            throw error
+        }
+
+    }
+
+    async deletePost (slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwritDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+            )        
+            return true;    
+        } catch (error) {
+            throw error
+            return false;
+        }
+
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwritDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            throw error
+            return false;
+        }
+    }
+
+    async getPosts (queries = [Query.equal("status", "active")]){
+        try {
+            await this.databases.listDocuments(
+                conf.appwritDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async uploadFile(file){
+        try {
+             return await this.bucket.createFile(
+                conf.appwriteBuketId,
+                ID.unique(),
+                file
+             )       
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async deleteFIle(fileID){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBuketId,
+                fileID
+            )            
+        } catch (error) {
+            throw error
+        }
+    }
+
+    getFilePreview(fileID){
+        return this.bucket.getFilePreview(
+            conf.appwriteBuketId,
+            fileID
+        )
+    }
+
+    
 }
+
 
 const service = new Service()
 export default service
